b747208e112b8f1df70bfb47bb254f48c85198c4
andronj <andstepanenko@tut.by>
2022-10-11 18:57:26 +0300

    new_card

---
+++ src/js/cards.js
@@ -0,0 +1,7 @@
+const cardRoot = document.querySelector('.cards-root');
+const ElementCard = document.createElement('div');
+const ElementCardQuickScreen = document.createElement('a');
+
+
+ElementCard.classList.add('cardelement');
+cardRoot.appendChild(ElementCard);
